a27b7d8f5b6998c00900df63d5247f140df36828
digitradiart <digitradiart@gmail.com>
2022-11-27 09:04:38 +0700

    show and hide admin profile

---
+++ src/script.js
@@ -0,0 +1,16 @@
+const sl_ap = document.getElementById("sl_ap");
+
+sl_ap.style.opacity = 0;
+sl_ap.style.visibility = "hidden";
+
+function showAdminProfile() {
+  sl_ap.style.opacity = 1;
+  sl_ap.style.visibility = "visible";
+  sl_ap.style.transition = "all 0.8s";
+}
+
+function hideAdminProfile() {
+  sl_ap.style.opacity = 0;
+  sl_ap.style.visibility = "hidden";
+  sl_ap.style.transition = "all 5s";
+}
